42ccfe62d7f2848e3de0048759f09546f88a55fb
Guilherme Nunes <guilhermenunes09@protonmail.com>
2023-02-04 21:06:21 -0400

    style update

--- src/components/product.js
+++ src/components/product.js
@@ -9,15 +9,18 @@ const Button = ({ to, children, className }) => (
 
 const Product = ({ product }) => {
   return (
-    <div className="card mb-3 mx-auto" style={{maxWidth: 600}}>
+    <div className="card mb-3 mx-auto" style={{maxWidth: 300}}>
       <img src={product.thumbnail} alt={product.title} className="card-img-top" />
       <div className="card-body">
         <h5 className="card-title">{product.title}</h5>
         <p className="card-text">{product.description}</p>
-        <div className="d-flex justify-content-center">
-          <Button to={`/products/${product.id}`} className="btn-primary m-2">Ver dados</Button>
-          <Button to={`/products/edit/${product.id}`} className="btn-warning m-2">Alterar</Button>
-          <button className="btn btn-danger m-2">Excluir</button>
+        
+      </div>
+      <div className='card-footer'>
+        <div className="d-flex justify-content-center align-items-center">
+          <Button to={`/products/${product.id}`} className="btn m-1">Ver dados</Button>
+          <Button to={`/products/edit/${product.id}`} className="btn m-1">Alterar</Button>
+          <button className="btn m-1">Excluir</button>
         </div>
       </div>
     </div>
--- src/components/productIndex.js
+++ src/components/productIndex.js
@@ -4,12 +4,14 @@ import Product from "./product";
 const ProductIndex = ({ products }) => {
   return (
     <>
-      <h1 className="text-center">List of Products</h1>
-      {
-        products && products.map((product) => (
-          <Product key={product.id} product={product} />
-        ))
-      }
+      <h1 className="text-center">E-commerce</h1>
+      <div class="d-flex align-content-start flex-wrap">
+        {
+          products && products.map((product) => (
+            <Product key={product.id} product={product} />
+          ))
+        }
+      </div>
     </>
   )
 }
